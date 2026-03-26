23a8ea0d2c49e195d2acc343d5e6bd53594c528a
kl00135 <144936829+kl00135@users.noreply.github.com>
2026-03-26 09:34:54 -0400

    Initial commit with Visual Studio solution and folders

---
+++ Furniture4AllApp/Views/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Furniture4AllApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
