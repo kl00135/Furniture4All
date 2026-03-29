ad5d7ef9b8a2117b06e601234eb755d24f5c8ef3
kl00135 <144936829+kl00135@users.noreply.github.com>
2026-03-29 13:01:29 -0400

    Populated Employee model with needed fields

--- Furniture4AllApp/Models/Employee.cs
+++ Furniture4AllApp/Models/Employee.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Furniture4AllApp.Models
 {
-    internal class Employee
+    public class Employee
     {
+        public int EmployeeID { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
