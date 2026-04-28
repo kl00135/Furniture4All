29bd7702c25daeecf1cbdd78b6097fb72efbd849
kl00135 <144936829+kl00135@users.noreply.github.com>
2026-04-27 23:09:38 -0400

    Added script to update password as a hash

---
+++ Furniture4AllApp/Script.PostDeployment.sql
@@ -0,0 +1,5 @@
+﻿UPDATE [dbo].[Employee]
+SET [password_hash] = CONVERT(VARCHAR(100), HASHBYTES('SHA2_256', [password_hash]), 2)
+WHERE [password_hash] IS NOT NULL 
+  AND [password_hash] NOT LIKE '0x%';
+GO
